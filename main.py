b18b5f109d8f8fe398f6ca5df5c384b4d1ff785b
vibikerski <vibikerski@gmail.com>
2023-11-03 23:36:40 +0100

    Базова версія

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import pygame
+from settings import *
+from scenes import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Battle City Remake")
+
+clock = pygame.time.Clock()
+menu = Menu(screen)
+
+IS_RUNNING = True
+while IS_RUNNING:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            IS_RUNNING = False
+    
+    if CURRENT_STATE == MENU_STATE:
+        menu.render()
+        # CURRENT_STATE = menu.handle(events) - можливо, зміна стану від інтеракції
+
+    clock.tick(FPS)
+    pygame.display.update()
+    
